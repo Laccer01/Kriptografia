e8b6d1878686df904898517d7779a9314766a4a4
Velican László <velicanlaszlo01@gmail.com>
2023-01-07 18:20:18 +0200

    MHK encrypt-descrypt almost working

--- assign3/Merkle-Hellman-Knapsack.py
+++ assign3/Merkle-Hellman-Knapsack.py
@@ -1,24 +1,46 @@
 import random
 import math
 import utils
+import itertools
+
+def getAllCombinations(lista):
+    combinations = []
+    for r in range(len(lista)+1):
+        for combination in itertools.combinations(set(lista), r):
+            combinations.append(combination)
+    return combinations
+
+def are_coprime(a,b):
+    
+    hcf = 1
+
+    for i in range(1, a+1):
+        if a%i==0 and b%i==0:
+            hcf = i
+
+    return hcf == 1
+
+def prime (x):
+    if(x > 1): 
+        for k in range(2, int(math.sqrt(x)) + 1): 
+            if (x % k == 0): 
+                return False;
+                
+    return True;
+
 
 def findCoprimePair(N):
 
-    for x in range(2, int(math.sqrt(N)) + 1):
-        if (N % x == 0):
-            while (N % x == 0):
-                N //= x
- 
-            if (N > 1):
- 
-                return x
+    for x in range(int(math.sqrt(N)) + 1,2,-1):
+        if (are_coprime(N,x) and prime(x)):
+            return x;
 
 def generate_private_key(n=8):
     global w, q, r
 
     w = [1]
     z = 1
-    for i in range(n):
+    for i in range(0,n-1):
         total = sum(w[:])
         z = random.randint(total + 1, 2 * total)
         w.append(z)
@@ -38,9 +60,11 @@ def create_public_key(private_key):
     q=private_key[1];
     r=private_key[2];
     n=8;
-
+    # print (private_key)
+    # print ()
     for i in range (n):
         beta.append((r * w[i])%q)
+    # print (beta)
     return tuple(beta)
 
 def toBinary(a):
@@ -72,48 +96,91 @@ def encrypt_mh(message, public_key):
   
     chunks = [message[i:i+8] for i in range(0, len(message), 8)]
     #print (chunks)
-
+    chunksFull = []
     for chunk in chunks:
-        betuChunk=''
+        betuChunk=[]
         for betu in chunk:
             array = toBinary(betu)
             c=0;
             for i in range (0,8):
                 c=c+int(array[i])*int(public_key[i]);
-            betuChunk+=toString2(str(c))
-        print (betuChunk)
+            # print (c)
+            betuChunk.append(c)
+            # print (betuChunk)
+            # print (ord(betuChunk))
+            # print (toBinary((betuChunk)))
+        
+        return (betuChunk)
 
+def modInverse(A, M):
+     
+    for X in range(1, M):
+        if (((A % M) * (X % M)) % M == 1):
+            return X
+    return -1
 
-    #raise NotImplementedError  # Your implementation here
+def visszaSzamol (kombinaciok, szam):
+    megfelel = False;
+    # szamMasolat=szam;
+    for lista in kombinaciok:
+        szamMasolat=szam;
+        for j in lista:
+            szamMasolat=szamMasolat-j;
+        if (szamMasolat==0):
+            return lista
+            break;
+        
+        
+def kialakitEredmeny (listaPrivateKey, megfeleloLista):
+    megoldas=''
+    for szam in listaPrivateKey:
+        if (szam in megfeleloLista):
+            megoldas+='1';
+        else:
+            megoldas+='0'
+    return megoldas;
+
 
 def decrypt_mh(message, private_key):
-    """Decrypt an incoming message using a private key
-    1. Extract w, q, and r from the private key
-    2. Compute s, the modular inverse of r mod q, using the
-        Extended Euclidean algorithm (implemented at `utils.modinv(r, q)`)
-    3. For each byte-sized chunk, compute
-         c' = cs (mod q)
-    4. Solve the superincreasing subset sum using c' and w to recover the original byte
-    5. Reconsitite the encrypted bytes to get the original message back
-    @param message Encrypted message chunks
-    @type message list of ints
-    @param private_key The private key of the recipient
-    @type private_key 3-tuple of w, q, and r
-    @return bytearray or str of decrypted characters
-    """
-
+    
     w=private_key[0];
     q=private_key[1];
     r=private_key[2];
-    s = utils.modinv(r,q)
-    raise NotImplementedError  # Your implementation here
+    #print (r,q)
+    s = modInverse(r,q)
+    #print (s)
+    # print (list(w))
+    chunks = [message[i:i+8] for i in range(0, len(message), 8)]
+    for chunk in chunks:
+        betuChunk=''
+        for betuErtek in chunk:
+            szam=(betuErtek*s)%q;
+            # print (szam)
+            #itt kene visszalakitani a privat kulccsal a binaris kodot
+            komb = (getAllCombinations(list(w)))
+            megfeleloTomb = (visszaSzamol(komb,szam));
+            eredmeny = ((kialakitEredmeny(list(w), megfeleloTomb)));
+            betuChunk+= (toString2(str(eredmeny)))
+
+    return betuChunk
 
 privKey = (generate_private_key())
 publicKey = (create_public_key(privKey));
 
-#print (privKey,"\n",publicKey)
+# print (privKey,"\n",publicKey)
+#print (modInverse(50 ,2443))
 
-encrypt_mh("abracadabraaaaaas",publicKey)
-
-#xorByteStringWithKey("aha",2);
 #print (toString2('01100001'))
+
+# print("\n\n")
+kar=encrypt_mh("abcdefgh",publicKey)
+# print ('a', ord('a'))
+print (kar)
+kar2 = decrypt_mh(kar,privKey)
+print (kar2)
+
+#print (kar)
+#xorByteStringWithKey("aha",2);
+
+# komb = (getAllCombinations([1,2,3]))
+# print (visszaSzamol(komb,3));
